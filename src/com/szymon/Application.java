f2cb8abef3601816b5ef87dae9582dc18f59d7ac
nowszy94 <nowszy94@gmail.com>
2017-06-06 18:28:03 +0200

    Some other lambda examples

--- src/com/szymon/Application.java
+++ src/com/szymon/Application.java
@@ -1,9 +1,7 @@
 package com.szymon;
 
-import com.szymon.builder.User;
-
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class Application {
@@ -21,12 +19,44 @@ public class Application {
         List<Action> actions = Arrays.asList(action, action1);
 
         List<String> names = Arrays.asList("Szymon", "Adam", "Anna");
+
+        List<List<String>> nameList = Arrays.asList(
+                Arrays.asList("Szymon", "Wojciech", "Adam"),
+                Arrays.asList("Anna", "Janina", "Malgorzata"));
+
+        System.out.println("List");
+        nameList.stream()
+                .flatMap(e -> e.stream())
+                .filter(new Predicate<String>() {
+                    @Override
+                    public boolean test(String s) {
+                        return s.startsWith("A");
+                    }
+                })
+                .filter(e -> e.startsWith("A"))
+                .filter(UserPredicateFactory.nameStartsWith("A"))
+                .sorted()
+                .forEach(e -> System.out.println(e));
+
+        Map<String, List<String>> map = new HashMap<>();
+        map.put("men", Arrays.asList("Szymon", "Wojciech", "Adam"));
+        map.put("woman", Arrays.asList("Anna", "Janina", "Malgorzata"));
+
+        System.out.println("\nMap");
+        map.entrySet()
+                .stream()
+                .map(e -> e.getValue())
+                .flatMap(e -> e.stream())
+                .filter(UserPredicateFactory.nameStartsWith("A"))
+                .sorted()
+                .forEach(e -> System.out.println(e));
+
         names.stream()
                 .filter(new Blabla())
                 .flatMap(e -> Arrays.asList(e.split("")).stream())
                 .distinct()
-                .sorted()
-                .forEach(System.out::println);
-        actions.forEach(e -> e.doAction());
+                .sorted();
+//                .forEach(System.out::println);
+//        actions.forEach(e -> e.doAction());
     }
 }
